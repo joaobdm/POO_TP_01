31619fefb41a09c01c74fb6cfdeec6f3c8067b1c
JP Berger <joaopaulobmata@gmail.com>
2021-05-11 19:32:29 -0300

    Adição de região informativa na classe App

--- codigo/Parte_01/src/App.java
+++ codigo/Parte_01/src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-
+        
+        ////#region
+        //Testes referentes a classe Jogo e seus herdeiros
         Data hoje = new Data(11, 05, 2021);
         
         Playstation jg = new Playstation("God Of War", 75, hoje);     
@@ -11,5 +13,6 @@ public class App {
 
         Xbox jg3 = new Xbox("Battle Toads", 50, hoje);
         jg3.toString();
+        ////#endregion
     }
 }
