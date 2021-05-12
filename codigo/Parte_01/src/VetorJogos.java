2cb41ea5e391361593dcc39b03b0acf1c0105be9
João Paulo Berger da Mata <joaopaulobmata@gmail.com>
2021-05-12 09:20:40 -0300

    Criação do método removerJogo e desenvolvimento da documentação da classe VetorJogos

--- codigo/Parte_01/src/VetorJogos.java
+++ codigo/Parte_01/src/VetorJogos.java
@@ -7,6 +7,12 @@ public class VetorJogos {
         jogos = new Jogo[1000];
     }
 
+    /**
+     * Através do nome e plataforma, detecta se o jogo já existe no vetor em questão.
+     * @param nomeDoJogo
+     * @param plataforma
+     * @return
+     */
     public boolean buscaJogo(String nomeDoJogo, String plataforma) {
 
         for (Jogo jogo : jogos) {
@@ -18,6 +24,11 @@ public class VetorJogos {
         return false;
     }
 
+    /**
+     * Adiciona um Jogo j ao vetor caso já não exista o mesmo jogo nesse vetor.
+     * @param j Jogo a ser inserido no vetor.
+     * @return true se a operação foi feita com sucesso, false caso contrário.
+     */
     public boolean adicionarJogo(Jogo j) {
         if (!buscaJogo(j.getNome(), j.getPlataforma())) {
             for (Jogo jogo : jogos) {
@@ -30,10 +41,27 @@ public class VetorJogos {
         return false;
     }
 
+    /**
+     * Remove um jogo do vetor e diminui uma posição de cada jogo posterior do vetor, se houver.
+     * @param posi Posição do vetor onde o jogo se encontra
+     * @return Jogo removido
+     */
+    public Jogo removerJogo(int posi) {
+        Jogo removido = null;
+        removido = jogos[posi];
+        jogos[posi] = null;
+        while (this.jogos[posi+1] != null){
+            jogos[posi] = jogos[posi+1];
+            posi++;
+            jogos[posi] = null;
+        }
+        return removido;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         for (Jogo jogo : jogos) {
-            while (jogo != null){
+            while (jogo != null) {
                 sb.append(jogo.toString());
             }
         }
