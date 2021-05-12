9b99544921dc2ea3367c59907a15e83ac6ce4684
João Paulo Berger da Mata <joaopaulobmata@gmail.com>
2021-05-12 09:20:55 -0300

    Criação classe Loja, pouco desenvolvida

---
+++ codigo/Parte_01/src/Loja.java
@@ -0,0 +1,9 @@
+public class Loja {
+    //private Cliente [] clietesCadastrados;
+    private VetorJogos bibliotecaJogos;
+    private VetorJogos carrinhoCompras;
+
+    Loja(){
+        bibliotecaJogos = new VetorJogos();
+    }
+}
